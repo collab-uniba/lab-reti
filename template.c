42c16aed1273f6c69df579d22db847d8394c0ecc
Fabio Calefato <fcalefato@gmail.com>
2018-10-14 15:52:10 +0200

    Update template.c

--- template.c
+++ template.c
@@ -12,27 +12,27 @@
 #endif
 
 #include <stdio.h>
-...
+//...
 
-int main(int argc, char *argv[]) { 
-  ...
-	#if defined WIN32
+int main(int argc, char *argv[]) {
+	//...
+#if defined WIN32
 	// Initialize Winsock
- 	WSADATA wsaData;
- 	int iResult =
-	   WSAStartup(MAKEWORD(2,2), &wsaData);
- 	if (iResult != 0) {
- 	   printf("Error at WSAStartup()\n");
-         return 0;
- 	}
-	#endif
-	int my_socket
+	WSADATA wsa_data;
+	int result =
+	WSAStartup(MAKEWORD(2,2), &wsa_data);
+	if (result != 0) {
+		printf("Error at WSAStartup()\n");
+		return 0;
+	}
+#endif
+	int my_socket;
 
-  ...
+	//...
 
-  closesocket(my_socket);
-  	#if defined WIN32
-         WSACleanup();
-  	#endif
-  	return 0;
+	closesocket(my_socket);
+#if defined WIN32
+	WSACleanup();
+#endif
+	return 0;
 } // main end
